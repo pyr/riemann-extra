ec25ad23172019df29c7565862335e90ebc54551
Pierre-Yves Ritschard <pyr@spootnik.org>
2013-06-18 10:50:13 +0200

    force source in project

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/pyr/riemann-extra"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :omit-source false)
