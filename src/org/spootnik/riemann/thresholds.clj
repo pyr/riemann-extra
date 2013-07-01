8b52d0d4323c90e668d5620821b888c2ada75849
Pierre-Yves Ritschard <pyr@spootnik.org>
2013-07-01 10:40:26 +0200

    add per host or per tag thresholds

--- src/org/spootnik/riemann/thresholds.clj
+++ src/org/spootnik/riemann/thresholds.clj
@@ -2,21 +2,36 @@
  "A common riemann use case: changing event states based
   on threshold lookups")
 
+(defn find-specific-threshold
+  [{:keys [host tags]}
+   {:keys [match-host match-tag match-default] :as threshold}]
+  (cond
+   match-tag     (and ((set tags) match-tag) threshold)
+   match-host    (and (= match-host host) threshold)
+   match-default threshold))
+
+(defn find-threshold
+  [thresholds event]
+  (if-let [thresholds (get thresholds (:service event))]
+    (if-let [(sequential? thresholds)]
+      (some (partial find-specific-threshold) thresholds)
+      thresholds)))
+
 (defn threshold-check-builder
   "Given a list of standard or inverted thresholds, yield
    a function that will adapt an inputs state.
 
    The output function does not process events with no metrics"
   [thresholds]
-  (let [pred (if (-> thresholds first val :invert) <= >)]
-    (fn [{:keys [metric service] :as event}]
-      (if-let [{:keys [warning critical]} (if metric (get thresholds service))]
-        (assoc event :state
-               (cond
-                (pred metric critical) "critical"
-                (pred metric warning)  "warning"
-                :else                  "ok"))
-        event))))
+  (fn [{:keys [metric service] :as event}]
+    (if-let [{:keys [warning critical invert]}
+             (if metric (find-threshold thresholds event))]
+      (assoc event :state
+             (cond
+              ((if invert <= >) metric critical) "critical"
+              ((if invert <= >) metric warning)  "warning"
+              :else                  "ok"))
+      event)))
 
 (defn threshold-check
   "Given a map of threshold associating a service name with :warning
