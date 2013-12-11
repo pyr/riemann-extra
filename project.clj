5976b4c41c4600337a62076eb77a245b77393320
Pierre-Yves Ritschard <pyr@spootnik.org>
2013-12-11 11:27:56 +0100

    allow compilation for testing

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "https://github.com/pyr/riemann-extra"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :profiles {:dev {:dependencies [[riemann "0.2.4"]]}}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :omit-source false)
