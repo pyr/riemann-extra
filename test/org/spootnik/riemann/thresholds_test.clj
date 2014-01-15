e4dafec678bcf7dbc8c64d2a84387e4b0d783e1a
Pierre-Yves Ritschard <pyr@spootnik.org>
2014-01-15 14:41:42 +0100

    fix tests, inspired by @lwf

--- test/org/spootnik/riemann/thresholds_test.clj
+++ test/org/spootnik/riemann/thresholds_test.clj
@@ -6,25 +6,28 @@
   {"cpu-user" {:warning 30 :critical 60}
    "cpu-system" {:warning 30 :critical 60}
    "cpu-idle2" {:warning 50 :critical 50 :invert true}
-   "cpu-nice" {:warning 50 :critical 20}   
+   "cpu-nice" {:warning 50 :critical 20}
    "cpu-idle" {:warning 50 :critical 20 :invert true}
    "cpu-idle3" {:warning 50 :critical 20 :invert true}
-   "cpu-steal" {:warning 50 :critical 20}})
+   "cpu-steal" {:warning 50 :critical 20}
+   "cpu-foo" {}})
 
 (deftest forward-threshold-test
   (testing "forward thresholds should match"
     (let [testfn (threshold-check thresholds)]
-      (is (= {:service "cpu-user" :metric 100 :state "critical"}
+      (is (= {:service "cpu-user" :metric 100 :state "critical" :tags #{}}
              (testfn {:service "cpu-user" :metric 100})))
-      (is (= {:service "cpu-user" :metric 50 :state "warning"}
+      (is (= {:service "cpu-user" :metric 50 :state "warning" :tags #{}}
              (testfn {:service "cpu-user" :metric 50})))
-      (is (= {:service "cpu-idle" :metric 0 :state "critical"}
+      (is (= {:service "cpu-idle" :metric 0 :state "critical" :tags #{}}
              (testfn {:service "cpu-idle" :metric 0})))
-      (is (= {:service "cpu-idle" :metric 40 :state "warning"}
+      (is (= {:service "cpu-idle" :metric 40 :state "warning" :tags #{}}
              (testfn {:service "cpu-idle" :metric 40})))
       (is (= {:service "foo"}
              (testfn {:service "foo"})))
       (is (= {:service "foo" :metric 2}
              (testfn {:service "foo" :metric 2})))
       (is (= {:service "cpu-idle"}
-             (testfn {:service "cpu-idle"}))))))
+             (testfn {:service "cpu-idle"})))
+      (is (= {:service "cpu-foo"}
+             (testfn {:service "cpu-foo"}))))))
